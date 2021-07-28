472116a07790fdaa15bfbeeb8af3767b8ec8f753
Will Boyd <willb@linuxacademy.com>
2021-07-28 12:33:34 -0400

    add final tweaks

--- src/pages/ListManager.js
+++ src/pages/ListManager.js
@@ -1,19 +1,16 @@
 import React, { Component, Fragment } from 'react';
-import { withRouter, Route, Redirect, Link } from 'react-router-dom';
+import { withRouter, Route, Link } from 'react-router-dom';
 import {
   withStyles,
   Typography,
-  IconButton,
   Paper,
   List,
   ListItem,
   ListItemText,
-  ListItemSecondaryAction,
 } from '@material-ui/core';
 import Fab from '@material-ui/core/Fab';
-import { Delete as DeleteIcon, Add as AddIcon } from '@material-ui/icons';
-import moment from 'moment';
-import { find, orderBy } from 'lodash';
+import { Add as AddIcon } from '@material-ui/icons';
+import { orderBy } from 'lodash';
 import { compose } from 'recompose';
 
 import ItemEditor from '../components/ItemEditor';
