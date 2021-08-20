19b7e96fa2491479ba0f73bfbba8d130d6cdd5c8
Will Boyd <willb@linuxacademy.com>
2021-08-20 13:54:06 -0400

    add config

--- src/server/index.js
+++ src/server/index.js
@@ -7,13 +7,13 @@ app.use(express.urlencoded({ extended: true }))
 // Setup MongoDb backing database
 const MongoClient = require('mongodb').MongoClient
 // MongoDB credentials
-const username = encodeURIComponent("uloe_user");
-const password = encodeURIComponent("ILoveTheList");
+const username = encodeURIComponent(process.env.MONGODB_USER || "uloe_user");
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD || "ILoveTheList");
 // MongoDB connection info
-const mongoPort = 27017;
-const mongoHost = 'localhost';
+const mongoPort = process.env.MONGODB_PORT || 27017;
+const mongoHost = process.env.MONGODB_HOST || 'localhost';
 // MongoDB connection string
-const mongoURI = `mongodb://${username}:${password}@localhost:27017/uloe`;
+const mongoURI = `mongodb://${username}:${password}@${mongoHost}:${mongoPort}/uloe`;
 const mongoURISanitized = `mongodb://${username}:****@${mongoHost}:${mongoPort}/uloe`;
 console.log("MongoDB connection string %s", mongoURISanitized);
 
